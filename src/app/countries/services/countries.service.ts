675c92c7969a2a059128de7a4fbd039028091c40
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-03-27 22:29:54 -0600

    second

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = "https://restcountries.com/v3.1";
+
+  constructor(private http: HttpClient) { }
+
+  //Metodo para buscar por capital
+  searchByCapital(term: string): Observable<Country[]> {
+    const url: string = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url);
+  }
+
+
+
+}
